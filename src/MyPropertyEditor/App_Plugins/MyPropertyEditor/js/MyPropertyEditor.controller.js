9fc7322d59093b978b2345879d6e5a645724ce15
GurayCoskun <guray@appart.com.tr>
2022-02-18 05:10:09 +0300

    seo

--- src/MyPropertyEditor/App_Plugins/MyPropertyEditor/js/MyPropertyEditor.controller.js
+++ src/MyPropertyEditor/App_Plugins/MyPropertyEditor/js/MyPropertyEditor.controller.js
@@ -1,4 +1,58 @@
-﻿function giphyPropertyEditorController($scope, $http) {
+﻿angular.module('umbraco').controller("seoEditorController", function ($scope, editorState) {
 
-}
-angular.module('umbraco').controller("myPropertyEditorController", giphyPropertyEditorController);
+    var currentNode = editorState.getCurrent();
+
+    if ($scope.model && $scope.model.value) {
+        $scope.title = $scope.model.value.title;
+        $scope.description = $scope.model.value.description;
+        $scope.keyword = $scope.model.value.keyword;
+    } else {
+
+        $scope.title = "";
+        $scope.description = "";
+        $scope.keyword = "";
+
+    }
+
+    $scope.model.value = { title: $scope.title, description: $scope.description, keyword: $scope.keyword };
+
+
+    $scope.$watch("title", function () {
+        $scope.UpdateModel();
+    });
+
+    $scope.$watch("description", function () {
+        $scope.UpdateModel();
+    });
+
+    $scope.$watch("keyword", function () {
+        $scope.UpdateModel();
+    });
+
+    $scope.UpdateModel = function () {
+        $scope.model.value = { title: $scope.title, description: $scope.description, keyword: $scope.keyword };
+    };
+
+    $scope.getTitle = function () {
+
+        var title = '';
+
+        if ($scope.title && $scope.title !== '') {
+            title = $scope.title;
+        }
+        else {
+            if (currentNode && currentNode.variants) {
+                title = currentNode.variants[0].name;
+            } else {
+                title = '';
+            }
+
+        }
+
+        // Only append suffix if we have a value to render.
+        if (title === '') {
+            return title;
+        }
+
+    };
+});
